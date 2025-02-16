05605005904dda8ffc853f8fc8c750190d80aa2d
Margus Muru <margusmuru@gmail.com>
2025-02-16 14:17:07 +0200

    init

---
+++ src/main/java/com/margusmuru/opentelemetrydemo/OpenTelemetryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.margusmuru.opentelemetrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTelemetryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTelemetryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/margusmuru/opentelemetrydemo/OpentelemetrydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margusmuru.opentelemetrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentelemetrydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
