5623165c8dbed5dd4eca79d1cbd01bc282f21254
Margus Muru <margusmuru@gmail.com>
2025-03-05 23:40:14 +0200

    logs work

--- src/main/java/com/margusmuru/opentelemetrydemo/HelloService.java
+++ src/main/java/com/margusmuru/opentelemetrydemo/HelloService.java
@@ -4,17 +4,22 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class HelloService {
     private final Tracer tracer;
 
     public String sayHello() {
+        log.info("Hello World!");
         Span span = tracer.spanBuilder("sayHello").startSpan();
         try (Scope scope = span.makeCurrent()) {
+            log.debug("Hello World debug!");
             sleep();
+            log.error("Hello World error!");
             return "Hello World";
         } finally {
             span.end();
